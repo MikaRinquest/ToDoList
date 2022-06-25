8b2160b343e48bbfd23bb74766ff5d5beb4a926b
Mika Rinquest <rinquestmika@gmail.com>
2022-06-25 23:24:54 +0200

    Mobile Responsive

--- styling.js
+++ styling.js
@@ -46,19 +46,54 @@ function addTask() {
   newObject = { id, item, createDate, completed };
   objectives.push(newObject);
   console.log(objectives);
-  // addToStorage(objectives);
+  addToStorage(objectives);
   showItems(objectives);
   document.querySelector("#task").value = " ";
 }
 
 // Editing a task 
-let edit = document.getElementById("content");
+let edit = document.getElementById("content"); //Chooses what can be edited
 function editBtn() {
-edit.contentEditable = true;
-edit.style.backgroundColor = "mediumorchid"
+edit.contentEditable = true; //states the the selected can be edited
+edit.style.backgroundColor = "mediumorchid" // changes background to show it can be edited
 }
+
+// Warning, currently only affects the first item in a list
 
 // Delete a task
 
+function deleteTask() {
+  const id = objectives.indexOf(name);
+  const removeTask = objectives.splice(id,1);
+  addToStorage(objectives);
+  showItems(objectives);
+}
+
+// Sort
+function sortAsc() {
+  objectives.sort((a,b) => {
+    if (a.item.toLowerCase() < b.item.toLowerCase()) {
+      return -1;
+    }
+    if (a.item.toLowerCase() > b.item.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
+  showItems(objectives);
+}
+
+function sortDesc() {
+  objectives.sort((a,b) => {
+    if (b.item.toLowerCase() < a.item.toLowerCase()) {
+      return -1;
+    }
+    if (b.item.toLowerCase() > a.item.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
+  showItems(objectives);
+}
 
 
